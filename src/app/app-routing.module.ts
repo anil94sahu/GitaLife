4438818a535e28f5bdd58ce06cce07854325ed31
lohith4694 <lohith.korada@gramenerit.com>
2019-02-26 19:19:19 +0530

    gallery updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,10 +8,11 @@ const routes: Routes = [
   { path: 'contact-us', loadChildren: './contact-us/contact-us.module#ContactUsPageModule' },
   { path: 'time-table', loadChildren: './time-table/time-table.module#TimeTablePageModule' },
   { path: 'gallery', loadChildren: './gallery/gallery.module#GalleryPageModule' },
-  { path: 'home', loadChildren: './home/home.module#HomePageModule' }
+  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
+  
 })
 export class AppRoutingModule {}
--- src/app/gallery/gallery.page.ts
+++ src/app/gallery/gallery.page.ts
@@ -6,7 +6,26 @@ import { MenuController } from '@ionic/angular';
   styleUrls: ['./gallery.page.scss'],
 })
 export class GalleryPage implements OnInit {
-
+  items = [
+    {
+      imageUrl: 'assets/img/lists/stadium.jpg',
+      title: 'Books',
+      place: 'Madison Square',
+      date: '05/06/2016'
+    },
+    {
+      imageUrl: 'assets/img/lists/stadium-3.png',
+      title: 'Kirtan',
+      place: 'Hooli',
+      date: '15/03/2016'
+    },
+    {
+      imageUrl: 'assets/img/lists/stadium-2.jpg',
+      title: 'Video',
+      place: 'Castelão',
+      date: '05/12/2015'
+    },
+  ];
   constructor(private menu: MenuController) {
    }
 
@@ -25,6 +44,18 @@ export class GalleryPage implements OnInit {
   openCustom() {
     this.menu.enable(true, 'custom');
     this.menu.open('custom');
+  }
+
+  delete(item) {
+    alert('Deleted ' + item.title);
+  }
+
+  viewComments(item) {
+    alert('Viewing comments of ' + item.title);
+  }
+
+  viewPlayers(item) {
+    alert('Viewing players of ' + item.title);
   }
 
 }
